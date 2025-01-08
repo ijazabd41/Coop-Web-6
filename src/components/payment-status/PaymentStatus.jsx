b3218177963bd0398af273358dfb533d64a7e46b
WRTeam <ahirmohan101@gmail.com>
2025-01-08 12:36:13 +0530

    improve success page

--- src/components/payment-status/PaymentStatus.jsx
+++ src/components/payment-status/PaymentStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router";
 import OrderSuccessModal from '../ordersuccessmodal/OrderSuccessModal';
 import * as api from "@/api/apiRoutes"
