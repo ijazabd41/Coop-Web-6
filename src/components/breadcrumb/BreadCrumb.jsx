bd4c9fb02af20c2c8c78579baa245c498e58390d
BurhanWRTeam <burhanwrteam5253@gmail.com>
2025-01-22 19:27:18 +0530

    Modified: BreadCrumb Component

--- src/components/breadcrumb/BreadCrumb.jsx
+++ src/components/breadcrumb/BreadCrumb.jsx
@@ -3,10 +3,14 @@ import { useRouter } from 'next/router';
 import { FaChevronRight } from 'react-icons/fa';
 import Link from 'next/link';
 
+const notFoundRoute = [
+    "/order-detail",
+]
+
 const BreadCrumb = () => {
     const router = useRouter();
     const [breadcrumbs, setBreadcrumbs] = useState([]);
-    const { slug } = router.query;
+    // const { slug } = router.query;
     useEffect(() => {
         if (router.pathname) {
             const pathArray = router.asPath.split('?')[0].split('/').filter((path) => path);
@@ -18,12 +22,40 @@ const BreadCrumb = () => {
         }
     }, [router.pathname, router.asPath]);
 
+
+    const handleNotFoundRoutes = (href) => {
+        if (href === "/product") {
+            return router.push("/products")
+        }
+        if (href === "/categories") {
+            return router.push("/categories/all")
+        }
+        const notFound = notFoundRoute.includes(href)
+        if (notFound) {
+            return router.back();
+        }
+        return router.push(href);
+    };
+
+    const handleCheckBreadCrumb = () => {
+        // console.log(parseInt(slug))
+        if (breadcrumbs?.length === 2 && parseInt(breadcrumbs[breadcrumbs.length - 1]?.label)) {
+            return breadcrumbs?.[0]?.label;
+        }
+        if (breadcrumbs?.length === 1) {
+            return breadcrumbs?.[0]?.label;
+        }
+        return breadcrumbs[1]?.label
+    }
+
+
+
     return (
         <section className="p-3 md:p-6 breadCrumbBg">
             <div className=" container">
                 <div className='flex justify-between flex-col gap-1 md:flex-row'>
                     <p className="text-xl font-bold capitalize">
-                        {breadcrumbs.length ? slug ? breadcrumbs[breadcrumbs.length - 2]?.label : breadcrumbs[breadcrumbs.length - 1]?.label : 'Home'}
+                        {breadcrumbs.length ? handleCheckBreadCrumb() : 'Home'}
                     </p>
                     <div className="flex gap-1 items-center overflow-hidden ">
                         <Link href="/" className="text-sm font-bold capitalize">
@@ -42,14 +74,15 @@ const BreadCrumb = () => {
                                         {crumb.label}
                                     </span>
                                 ) : (
-                                    <Link
-                                        href={crumb?.href === "/product" ? "/products" : crumb.href === "/categories" ? "/categories/all" : crumb.href}
-                                        className="text-sm font-bold capitalize text-ellipsis overflow-hidden whitespace-nowrap"
+                                    <div
+                                        // href={handleNotFoundRoutes(crumb.href)}
+                                        onClick={() => handleNotFoundRoutes(crumb.href)}
+                                        className="text-sm font-bold capitalize text-ellipsis overflow-hidden whitespace-nowrap cursor-pointer"
                                         style={{ maxWidth: '100%' }}
                                         title={crumb.label} // Tooltip to show full text on hover
                                     >
                                         {crumb.label}
-                                    </Link>
+                                    </div>
                                 )}
                             </div>
 
