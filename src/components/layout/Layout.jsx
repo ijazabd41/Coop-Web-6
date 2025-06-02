43e74a6d34484e27332402021edb21b781973a35
WRTeam <ahirmohan101@gmail.com>
2025-06-02 13:22:48 +0530

    location issue solved

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -11,27 +11,27 @@ import Loader from "../loader/Loader";
 import Location from "../locationmodal/Location";
 import { setFavoriteProductIds } from "@/redux/slices/FavoriteSlice";
 import PushNotification from "../firebasenotification/PushNotification";
-import LangFile from "@/utils/en.json"
-import { setAvailableLanguages, setSelectedLanguage } from "@/redux/slices/languageSlice";
+import LangFile from "@/utils/en.json";
+import {
+  setAvailableLanguages,
+  setSelectedLanguage,
+} from "@/redux/slices/languageSlice";
 import { useRouter } from "next/router";
 import MaintanceMode from "../error/MaintanceMode";
 
 const Layout = ({ children }) => {
   const router = useRouter();
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.User)
   const theme = useSelector((state) => state.Theme.theme);
   const setting = useSelector((state) => state.Setting);
-  const city = useSelector((state) => state.City);
-  const language = useSelector(state => state.Language.selectedLanguage)
+  const language = useSelector((state) => state.Language.selectedLanguage);
 
   const [loading, setLoading] = useState(false);
-  const [isAuthorized, setIsAuthorized] = useState(true);
   // const [showLocation, setShowLocation] = useState(false)
 
   useEffect(() => {
-    document.documentElement.dir = language?.type
-  }, [])
+    document.documentElement.dir = language?.type;
+  }, []);
 
   useEffect(() => {
     fetchSetting();
@@ -39,74 +39,90 @@ const Layout = ({ children }) => {
     fetchLanguage();
   }, []);
 
-
-
   const fetchLanguage = async () => {
     try {
-      const response = await api.getSystemLanguages({ id: 0, isDefault: 0, systemType: 3 })
+      const response = await api.getSystemLanguages({
+        id: 0,
+        isDefault: 0,
+        systemType: 3,
+      });
       if (response.status == 1) {
         if (response.data !== undefined) {
           if (response?.data?.length == 1) {
             try {
-              const langRes = await api.getSystemLanguages({ id: response?.data?.[0]?.id, isDefault: 1, systemType: 3 })
+              const langRes = await api.getSystemLanguages({
+                id: response?.data?.[0]?.id,
+                isDefault: 1,
+                systemType: 3,
+              });
               if (langRes.status == 1) {
-                document.documentElement.dir = langRes?.data?.type
-                dispatch(setSelectedLanguage({ data: langRes?.data }))
+                document.documentElement.dir = langRes?.data?.type;
+                dispatch(setSelectedLanguage({ data: langRes?.data }));
               } else {
                 const language = {
-                  "id": 15,
-                  "name": "English",
-                  "code": "en",
-                  "type": "LTR",
-                  "system_type": 3,
-                  "is_default": 1,
-                  "json_data": LangFile,
-                  "display_name": "English",
-                  "system_type_name": "Website"
-                }
-                dispatch(setSelectedLanguage({ data: language }))
+                  id: 15,
+                  name: "English",
+                  code: "en",
+                  type: "LTR",
+                  system_type: 3,
+                  is_default: 1,
+                  json_data: LangFile,
+                  display_name: "English",
+                  system_type_name: "Website",
+                };
+                dispatch(setSelectedLanguage({ data: language }));
               }
             } catch (error) {
-              console.log("error")
+              console.log("error");
             }
           } else if (language == null) {
-            const langId = response?.data?.find((lang) => lang?.is_default == 1)?.id;
-            const langRes = await api.getSystemLanguages({ id: langId, isDefault: 1, systemType: 3 })
-            document.documentElement.dir = langRes?.data?.type
-            dispatch(setSelectedLanguage({ data: langRes?.data }))
+            const langId = response?.data?.find(
+              (lang) => lang?.is_default == 1,
+            )?.id;
+            const langRes = await api.getSystemLanguages({
+              id: langId,
+              isDefault: 1,
+              systemType: 3,
+            });
+            document.documentElement.dir = langRes?.data?.type;
+            dispatch(setSelectedLanguage({ data: langRes?.data }));
           }
-          dispatch(setAvailableLanguages({ data: response.data }))
+          dispatch(setAvailableLanguages({ data: response.data }));
         } else {
           const language = {
-            "id": 15,
-            "name": "English",
-            "code": "en",
-            "type": "LTR",
-            "system_type": 3,
-            "is_default": 1,
-            "json_data": LangFile,
-            "display_name": "English",
-            "system_type_name": "Website"
-          }
-          dispatch(setSelectedLanguage({ data: language }))
+            id: 15,
+            name: "English",
+            code: "en",
+            type: "LTR",
+            system_type: 3,
+            is_default: 1,
+            json_data: LangFile,
+            display_name: "English",
+            system_type_name: "Website",
+          };
+          dispatch(setSelectedLanguage({ data: language }));
         }
       }
     } catch (error) {
-      console.log("Error", error)
+      console.log("Error", error);
     }
-  }
+  };
 
   const fetchSetting = async () => {
     setLoading(true);
     try {
       const res = await api.getSetting();
-      const setting = JSON.parse(atob(res.data))
+      const setting = JSON.parse(atob(res.data));
       dispatch(setSetting({ data: setting }));
-      dispatch(
-        setFavoriteProductIds({ data: setting?.favorite_product_ids })
+      dispatch(setFavoriteProductIds({ data: setting?.favorite_product_ids }));
+      document.documentElement.style.setProperty(
+        "--primary-color",
+        setting?.web_settings?.color,
       );
-      document.documentElement.style.setProperty("--primary-color", setting?.web_settings?.color);
-      document.documentElement.style.setProperty("--light-primary-color", setting?.web_settings?.light_color);
+      document.documentElement.style.setProperty(
+        "--light-primary-color",
+        setting?.web_settings?.light_color,
+      );
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -125,7 +141,6 @@ const Layout = ({ children }) => {
     }
   };
 
-
   useEffect(() => {
     // Show loader on route change start
     const handleStart = () => setLoading(true);
@@ -143,25 +158,27 @@ const Layout = ({ children }) => {
     };
   }, [router]);
 
-
-
   return (
     <section>
-      {loading ? <Loader screen="full" /> :
-        setting?.setting?.web_settings?.website_mode == 1 ? <MaintanceMode message={setting?.setting?.web_settings?.website_mode_remark} /> :
-          <PushNotification>
-            <Header />
-            {children}
-            <Footer />
-            <ToastContainer
-              theme={theme}
-              key="toastContainer"
-              bodyClassName={"toast-body"}
-              toastClassName="toast-container-className"
-            />
-          </PushNotification>
-
-      }
+      {loading ? (
+        <Loader screen="full" />
+      ) : setting?.setting?.web_settings?.website_mode == 1 ? (
+        <MaintanceMode
+          message={setting?.setting?.web_settings?.website_mode_remark}
+        />
+      ) : (
+        <PushNotification>
+          <Header />
+          {children}
+          <Footer />
+          <ToastContainer
+            theme={theme}
+            key="toastContainer"
+            bodyClassName={"toast-body"}
+            toastClassName="toast-container-className"
+          />
+        </PushNotification>
+      )}
       {/* <Location showLocation={showLocation} setShowLocation={setShowLocation} /> */}
     </section>
   );
