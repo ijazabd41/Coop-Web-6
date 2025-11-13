335d704c6730111b292daa84b84613c7c5d3167a
WRTeam <ahirmohan101@gmail.com>
2025-11-13 10:49:30 +0530

    add MetaData component to requested products page

--- src/pages/profile/requested-products/index.jsx
+++ src/pages/profile/requested-products/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import MetaData from "@/components/metadata-component/MetaData";
 const RequestProducts = dynamic(
   () => import("@/components/pagecomponents/RequestedProductPage"),
   {
@@ -10,6 +11,10 @@ const RequestProducts = dynamic(
 const index = () => {
   return (
     <div>
+      <MetaData
+        pageName="/profile/requested-products"
+        title={`Requested Products - ${process.env.NEXT_PUBLIC_META_TITLE}`}
+      />
       <RequestProducts />
     </div>
   );
